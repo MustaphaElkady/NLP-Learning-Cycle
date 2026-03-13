4d07679f19415c8e8da2797f86981f94393dbc6a
Mustafa Elkady <MustaphaElkady@gmail.com>
2026-03-13 22:57:05 +0200

    Upload dataset

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,3 +1,10 @@
+# UI
+# ↓
+# call inference
+# ↓
+# display results
+
+
 import streamlit as st
 import sys
 import os
@@ -6,6 +13,9 @@ from inference import load_model_and_vocab, predict_next_word
 
 # select model and dataset
 st.header("NLP Learning Sycle")
+
+st.sidebar.title("Configrations")
+
 model_type = st.selectbox("Choose Model",["RNN", "LSTM", "TRANSFORMER"])
 dataset_type = st.selectbox("Select dataset size", ["small", "BIG dataset"])
 sentence = st.text_input("Enter sentence to test the model")
@@ -13,6 +23,20 @@ sentence = st.text_input("Enter sentence to test the model")
 
 # TESt model 
 if st.button("Test Model"):
-    model, vocab = load_model_and_vocab(model_type)
-    word = predict_next_word(model,vocab,sentence)
-    st.success(f"Output: {word}")
+    with st.spinner("computing..."):
+        model, vocab = load_model_and_vocab(model_type)
+        word = predict_next_word(model,vocab,sentence)
+        st.success(f"Output: {word}")
+
+
+
+uploaded_file = st.sidebar.file_uploader(
+    "Upload Dataset",
+    type=["txt"]
+)
+
+if uploaded_file is not None:
+
+    text = uploaded_file.read().decode("utf-8")
+
+    st.sidebar.write("Dataset Loaded ✅")
